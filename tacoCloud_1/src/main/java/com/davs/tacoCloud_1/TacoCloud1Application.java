981c19c4d4148c67e0f36a4aa68f6852e13955a7
DAVS <dvidelasilva@gmail.com>
2024-11-16 22:04:32 -0300

    fin seccion 1

---
+++ tacoCloud_1/src/main/java/com/davs/tacoCloud_1/TacoCloud1Application.java
@@ -0,0 +1,13 @@
+package com.davs.tacoCloud_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloud1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloud1Application.class, args);
+	}
+
+}
---
+++ tacoCloud_1/src/test/java/com/davs/tacoCloud_1/TacoCloud1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davs.tacoCloud_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloud1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
